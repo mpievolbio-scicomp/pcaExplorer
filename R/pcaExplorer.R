cc4b00c19965d0061f578f52de205a41711b584c
Federico Marini <marinif@uni-mainz.de>
2016-11-11 17:47:21 +0100

    call explicit shinyBS to load correctly the bsTooltips

--- R/pcaExplorer.R
+++ R/pcaExplorer.R
@@ -55,6 +55,8 @@ pcaExplorer <- function(dds=NULL,
          install.packages('shiny')")
   }
 
+  library("shinyBS") # to correctly display the bsTooltips
+
   # get modes and themes for the ace editor
   modes <- shinyAce::getAceModes()
   themes <- shinyAce::getAceThemes()
